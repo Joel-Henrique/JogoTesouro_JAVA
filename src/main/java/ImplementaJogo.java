f56e6312772c0153546f58153dd245322513b317
Joel-Henrique <joel.henrique@estudante.ufjf.br>
2022-06-21 18:57:03 -0300

    adiciona class jogador e coloca jogador no tabuleiro

--- src/main/java/ImplementaJogo.java
+++ src/main/java/ImplementaJogo.java
@@ -1,5 +1,5 @@
 public class ImplementaJogo {
-    int[][] Tabuleiro = new int[8][8];
+    private int[][] Tabuleiro = new int[8][8];
     public ImplementaJogo(){
         for (int lin = 0; lin < Tabuleiro.length; lin++) {
             for (int col = 0; col < Tabuleiro[0].length; col++) {
@@ -9,19 +9,35 @@ public class ImplementaJogo {
         for(int i= 0;i<10;i++)
             GeraTesouro();
 
+        adicionajogador();
+
 
     }
     private void GeraTesouro(){
         int n = (int) (java.lang.Math.random() * 8);
         int m = (int) (java.lang.Math.random() * 8);
-        //System.out.println(n);
-        //System.out.println(m);
         EspalhaTesouro(m,n);
 
     }
     private void EspalhaTesouro(int m,int n){
         Tabuleiro[m][n] = 7;
     }
+
+    public void andar(char c){
+
+    }
+
+    private void adicionajogador(){
+        int n = (int) (java.lang.Math.random() * 8);
+        int m = (int) (java.lang.Math.random() * 8);
+        Tabuleiro[m][n] = 9;
+    }
+
+
+
+
+
+
 
     public void imprimejogo(){
         for (int[] ints : Tabuleiro) {
--- src/main/java/Jogador.java
+++ src/main/java/Jogador.java
@@ -1,2 +1,11 @@
 public class Jogador {
+    private int experiencia = 0;
+
+    public void adicionaexp(){
+        experiencia++;
+    }
+    public int getexp(){
+        return experiencia;
+    }
+
 }
