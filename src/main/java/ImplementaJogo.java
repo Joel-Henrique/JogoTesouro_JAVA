991401feec2518fd374fe6fbd61a91eb8d33a512
Joel-Henrique <joel.henrique@estudante.ufjf.br>
2022-06-21 18:46:36 -0300

    adiciona e arruma GeraTesouro e EspalhaTesouro

--- src/main/java/ImplementaJogo.java
+++ src/main/java/ImplementaJogo.java
@@ -1,7 +1,39 @@
 public class ImplementaJogo {
-    public int Mat[][] = new int[8][8];
+    int[][] Tabuleiro = new int[8][8];
+    public ImplementaJogo(){
+        for (int lin = 0; lin < Tabuleiro.length; lin++) {
+            for (int col = 0; col < Tabuleiro[0].length; col++) {
+                Tabuleiro[lin][col] = 0;
+            }
+        }
+        for(int i= 0;i<10;i++)
+            GeraTesouro();
 
 
+    }
+    private void GeraTesouro(){
+        int n = (int) (java.lang.Math.random() * 8);
+        int m = (int) (java.lang.Math.random() * 8);
+        //System.out.println(n);
+        //System.out.println(m);
+        EspalhaTesouro(m,n);
+
+    }
+    private void EspalhaTesouro(int m,int n){
+        Tabuleiro[m][n] = 7;
+    }
+
+    public void imprimejogo(){
+        imprimejogo2(Tabuleiro);
+    }
+    private void imprimejogo2(int[][] Tabuleiro){
+        for (int[] ints : Tabuleiro) {
+            for (int col = 0; col < Tabuleiro[0].length; col++) {
+                System.out.print(ints[col] + " ");
+            }
+            System.out.println();
+        }
+    }
 
 
 }
--- src/main/java/JogoTesouro.java
+++ src/main/java/JogoTesouro.java
@@ -1,2 +1,6 @@
 public class JogoTesouro {
+    public static void main(String[] args) {
+        ImplementaJogo jogo = new ImplementaJogo();
+        jogo.imprimejogo();
+    }
 }
