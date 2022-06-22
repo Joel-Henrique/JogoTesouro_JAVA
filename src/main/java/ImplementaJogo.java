e94d93359455dc367e10ef8b94b41ee01145cace
Joel-Henrique <joel.henrique@estudante.ufjf.br>
2022-06-21 21:50:55 -0300

    algumas mudanças em relação ao construtor do jogo e ao modo como ele eh desenvolvido

--- src/main/java/ImplementaJogo.java
+++ src/main/java/ImplementaJogo.java
@@ -1,33 +1,58 @@
+import java.util.Scanner;
+
 public class ImplementaJogo {
-    private int tam= 8;
-    private int[][] Tabuleiro = new int[tam][tam];
+    private int tam;
+    private int qntdt;
     private Jogador jogador1 = new Jogador();
     private int[] posicaojogador = new int[2];
-    int qntdetesouros = 10;
-    public ImplementaJogo(){
+
+    public ImplementaJogo(int TamanhoDoTabuleiro,int qntdetesouros){
+        settamanho(TamanhoDoTabuleiro);
+        int[][] Tabuleiro = new int[tam][tam];
+        setQntdt(qntdetesouros);
+
         for (int lin = 0; lin < Tabuleiro.length; lin++) {
             for (int col = 0; col < Tabuleiro[0].length; col++) {
                 Tabuleiro[lin][col] = 0;
             }
         }
-        for(int i= 0;i<=qntdetesouros;i++)
-            GeraTesouro();
 
-        adicionajogador();
+
+        for (int i = 0; i < this.qntdt; i++)
+            GeraTesouro(Tabuleiro);
+
+        adicionajogador(Tabuleiro);
+
+        for (; jogador1.getexp() < this.qntdt ;){
+            Scanner teclado = new Scanner(System.in);
+            char anda;
+            System.out.print("Digite uma letra para andar: ");
+            anda = teclado.next().charAt(0);
+            System.out.println(anda);
+            andar(anda,Tabuleiro);
+            System.out.println();
+            //imprimejogo(Tabuleiro);
+        }
+    }
+
+    private void GeraTesouro(int[][] Tabuleiro){
+        int lin = (int) (java.lang.Math.random() * tam);
+        int col = (int) (java.lang.Math.random() * tam);
+        if((lin == 0) && (col ==0)){
+            for (;(lin == 0)&& (col ==0);) {
+                lin = (int) (java.lang.Math.random() * tam);
+                col = (int) (java.lang.Math.random() * tam);
+            }
+        }
+        EspalhaTesouro(lin,col,Tabuleiro);
 
     }
-    private void GeraTesouro(){
-        int lin = (int) (java.lang.Math.random() * 8);
-        int col = (int) (java.lang.Math.random() * 8);
-
-        EspalhaTesouro(lin,col);
-
-    }
-    private void EspalhaTesouro(int m,int n){
+    private void EspalhaTesouro(int m, int n, int[][] Tabuleiro){
         Tabuleiro[m][n] = 7;
     }
 
-    public void andar(char c){
+
+    public void andar(char c, int[][] Tabuleiro){
         int lin,col;
         lin = this.posicaojogador[0];
         col = this.posicaojogador[1];
@@ -39,9 +64,8 @@ public class ImplementaJogo {
                     if (lin >= 1) {
                         Tabuleiro[lin][col] = 0;
                         if (Tabuleiro[lin - 1][col] == 7) {
-                            System.out.println("Parabens seu novo nivel eh: " + (jogador1.getexp()+1));
+                            System.out.println("Parabéns, você encontrou um tesouro! Nova pontuação de experiência: "+ (jogador1.getexp()+1));
                             jogador1.adicionaexp();
-                            this.qntdetesouros--;
                         }
                         Tabuleiro[lin - 1][col] = 9;
                         posicaojogador[0] = lin - 1;
@@ -54,10 +78,8 @@ public class ImplementaJogo {
                     if (lin <= tam - 2) {
                         Tabuleiro[lin][col] = 0;
                         if (Tabuleiro[lin + 1][col] == 7) {
-                            System.out.println("Parabens seu novo nivel eh: " + (jogador1.getexp()+1));
+                            System.out.println("Parabéns, você encontrou um tesouro! Nova pontuação de experiência: "+ (jogador1.getexp()+1));
                             jogador1.adicionaexp();
-                            qntdetesouros--;
-
                         }
                         Tabuleiro[lin + 1][col] = 9;
                         posicaojogador[0] = lin + 1;
@@ -70,9 +92,8 @@ public class ImplementaJogo {
                     if (col >= 1) {
                         Tabuleiro[lin][col] = 0;
                         if (Tabuleiro[lin][col - 1] == 7) {
-                            System.out.println("Parabens seu novo nivel eh: " + (jogador1.getexp()+1));
+                            System.out.println("Parabéns, você encontrou um tesouro! Nova pontuação de experiência: "+ (jogador1.getexp()+1));
                             jogador1.adicionaexp();
-                            qntdetesouros--;
                         }
                         Tabuleiro[lin][col - 1] = 9;
                         posicaojogador[1] = col - 1;
@@ -83,15 +104,14 @@ public class ImplementaJogo {
                 }
             }
             case 'd' -> {
-                if (this.Tabuleiro[lin][col] >= 0) {
+                if (Tabuleiro[lin][col] >= 0) {
                     if (col <= (tam - 2)) {
-                        this.Tabuleiro[lin][col] = 0;
-                        if(this.Tabuleiro[lin][col+1] == 7) {
-                            System.out.println("Parabens seu novo nivel eh: " + (jogador1.getexp()+1));
+                        Tabuleiro[lin][col] = 0;
+                        if(Tabuleiro[lin][col+1] == 7) {
+                            System.out.println("Parabéns, você encontrou um tesouro! Nova pontuação de experiência: "+ (jogador1.getexp()+1));
                             jogador1.adicionaexp();
-                            qntdetesouros--;
                         }
-                        this.Tabuleiro[lin][col+1] = 9;
+                        Tabuleiro[lin][col+1] = 9;
                         this.posicaojogador[1] = col+1;
 
                     } else
@@ -110,22 +130,42 @@ public class ImplementaJogo {
                           4
      */
 
-    private void adicionajogador(){
-        int lin = (int) (java.lang.Math.random() * 8);
-        int col = (int) (java.lang.Math.random() * 8);
+    private void adicionajogador(int[][] Tabuleiro){
+        /*
+        int lin = (int) (java.lang.Math.random() * tam);
+        int col = (int) (java.lang.Math.random() * tam);
         this.posicaojogador[0] = lin;
         this.posicaojogador[1] = col;
-        this.Tabuleiro[lin][col] = 9;
+        Tabuleiro[lin][col] = 9;
+                    for (int i = 0; i <= this.qntdt; i++)
+                GeraTesouro(Tabuleiro);
+         */
+        Tabuleiro[0][0] = 9;
     }
 
 
-    public void imprimejogo(){
+    public void imprimejogo(int[][] Tabuleiro){
         for (int[] ints : Tabuleiro) {
             for (int col = 0; col < Tabuleiro[0].length; col++) {
                 System.out.print(ints[col] + " ");
             }
             System.out.println();
         }
+    }
+
+    private void setQntdt(int qntdetesouros) {
+
+        if(qntdetesouros > 0) {
+            this.qntdt = qntdetesouros;
+            //System.out.println("qnt dd: "+ this.qntdt);
+        }else
+            System.out.println("Quantidade de tesouros invalidos!!");
+    }
+    private void settamanho(int TamanhoDoTabuleiro){
+        if(TamanhoDoTabuleiro>=2){
+            this.tam = TamanhoDoTabuleiro;
+        }else
+            System.out.println("Tamanho Invalido!!");
     }
 
 }
--- src/main/java/JogoTesouro.java
+++ src/main/java/JogoTesouro.java
@@ -2,25 +2,14 @@ import java.util.Scanner;
 
 public class JogoTesouro {
     public static void main(String[] args) {
+
         Scanner teclado = new Scanner(System.in);
-        ImplementaJogo jogo = new ImplementaJogo();
-        jogo.imprimejogo();
-        System.out.println(jogo.qntdetesouros);
-
-
-        for (;jogo.qntdetesouros> 0;){
-            char anda;
-            System.out.print("Digite uma letra para andar: ");
-            anda = teclado.next().charAt(0);
-            System.out.println(anda);
-            jogo.andar(anda);
-            System.out.println("Quantidade de Tesouros restantes: "+ jogo.qntdetesouros);
-            System.out.println();
-            jogo.imprimejogo();
-        }
-
-        jogo.imprimejogo();
-
+        System.out.print("Informe a ordem da matriz (N x N): ");
+        int TamanhoDoTabuleiro = teclado.nextInt();
+        System.out.print("Informe a quantidade de tesouros: ");
+        int QuantdTesouros = teclado.nextInt();
+        //System.out.print("Informe o nome do jogador: ");
+        ImplementaJogo jogo = new ImplementaJogo(TamanhoDoTabuleiro,QuantdTesouros);
 
     }
 }
